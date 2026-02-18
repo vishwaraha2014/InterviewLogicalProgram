ff876aa31e74ceb68a213b589c9838b49c4bdb44
Bishwa Raha <vishwaraha2014@gmail.com>
2026-02-18 23:23:35 +0530

    Added RemoveDuplicateChars Logical Program..

---
+++ RemoveDuplicateString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RemoveDuplicateChars
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            string result = RemoveDuplicateChars(input);
+            Console.WriteLine(result);  // Output: "helo wrd"
+            Console.ReadLine();
+        }
+        public static string RemoveDuplicateChars(string str)
+        {
+            char[] chars = str.ToCharArray();
+            string result = "";
+            foreach (char c in chars)
+            {
+                if (!result.Contains(c))
+                {
+                    result += c;
+                }
+            }
+            return result;
+        }
+    }
+}
