7483c3a7bd0801e2b272d0e8fd8119626ac23785
Bishwa Raha <vishwaraha2014@gmail.com>
2026-02-23 12:36:34 +0530

    Added Logical program for StringList Count With Name.

---
+++ StringListCountWithName/StringListCountWithName.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringListCountWithName
+{
+    public class StringListCountWithName
+    {
+       private static readonly List<string> stringList = new List<string>{"ram","shyam", "ram"};
+
+        static void Main()
+        {
+            Dictionary<string, int> stringCount = new Dictionary<string, int>();
+            foreach (var name in stringList)
+            {
+                if (stringCount.ContainsKey(name))
+                {
+                    stringCount[name]++;
+                }
+                else
+                {
+                    stringCount[name] = 1;
+                }
+            }
+
+            foreach (var kvp in stringCount)
+            {
+                Console.WriteLine($"String: {kvp.Key}, Count: {kvp.Value}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
