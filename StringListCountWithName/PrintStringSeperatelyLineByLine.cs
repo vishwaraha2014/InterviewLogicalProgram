f8063142050cf7c37c19a19f88a916963230b8b6
Bishwa Raha <vishwaraha2014@gmail.com>
2026-03-03 22:35:57 +0530

    Print String Seperately Line By Line
    Add program to print each word of a string line by line
    
    Created PrintStringSeperatelyLineByLine.cs in the StringListCountWithName namespace. This program splits a hardcoded sentence into words and prints each word on a separate line, then waits for user input before exiting.

---
+++ StringListCountWithName/PrintStringSeperatelyLineByLine.cs
@@ -0,0 +1,19 @@
+﻿
+namespace StringListCountWithName
+{
+    public class PrintStringSeperatelyLineByLine
+    {
+
+        public static void Main(string[] args)
+        {
+            string str = "I am learning C# programming language";
+            string[] strArr = str.Split(" ");
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                Console.WriteLine(strArr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
