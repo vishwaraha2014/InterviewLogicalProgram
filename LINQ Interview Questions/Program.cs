05a6944b1d6ecc24a9327f6a6186e3cec2c7efda
Bishwa Raha <vishwaraha2014@gmail.com>
2026-03-28 21:03:55 +0530

    Issue Resolved

--- LINQ Interview Questions/Program.cs
+++ LINQ Interview Questions/Program.cs
@@ -1,59 +1,68 @@
-﻿//left Join ==>
-//from e employee
-//join d dept
-//on e.deptid equals d.id into deptGroup
-//from d in deptGroup.DefaultIfEmpty()
-//select	new
-//        {
-//            e.Name,
-//            DeptName = d != null ? d.Name : "No dept"
-//        };s
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+
+        //left Join ==>
+        //from e employee
+        //join d dept
+        //on e.deptid equals d.id into deptGroup
+        //from d in deptGroup.DefaultIfEmpty()
+        //select	new
+        //        {
+        //            e.Name,
+        //            DeptName = d != null ? d.Name : "No dept"
+        //        };s
 
 
-//RightJoin =>
-//from e employee
-//join d dept
-//on e.deptid equals d.id into empGroup
-//from e in empGroup.DefaultIfEmpty()
-//select new
-//{
-//    Name = e != null ? e.Name = "No Emp",
-//    DeptName = d.DeptName
-//};
+        //RightJoin =>
+        //from e employee
+        //join d dept
+        //on e.deptid equals d.id into empGroup
+        //from e in empGroup.DefaultIfEmpty()
+        //select new
+        //{
+        //    Name = e != null ? e.Name = "No Emp",
+        //    DeptName = d.DeptName
+        //};
 
 
-//normal Join ==>
-//from e employee
-//join d dept
-//on e.deptid euqals d.deptid 
-//select new
-//       {
-//           e.Name,
-//           d.DeptName
-//       };
+        //normal Join ==>
+        //from e employee
+        //join d dept
+        //on e.deptid euqals d.deptid 
+        //select new
+        //       {
+        //           e.Name,
+        //           d.DeptName
+        //       };
 
 
-//5th highest salary LINQ
-//public class Employee
-//{
-//    public int EmpId { get; set; }
-//    pubic decimal Salary { get; set; }
-//}
+        //5th highest salary LINQ
+        //public class Employee
+        //{
+        //    public int EmpId { get; set; }
+        //    pubic decimal Salary { get; set; }
+        //}
 
-//var 5thHighestSal = employees
-//					.Select(e => e.Salary)
-//					.Distinct()
-//					.OrderByDescending(s => s)
-//					.Skip(4)
-//					.FirstOrDefault();
+        //var 5thHighestSal = employees
+        //					.Select(e => e.Salary)
+        //					.Distinct()
+        //					.OrderByDescending(s => s)
+        //					.Skip(4)
+        //					.FirstOrDefault();
 
 
 
-//With rankedSalary as(
-//	select salary,
-//DENSE_RANK() OVER (ORDER BY SALARY DESC) AS RANKNUM
-//	FROM EMP
-//)
+        //With rankedSalary as(
+        //	select salary,
+        //DENSE_RANK() OVER (ORDER BY SALARY DESC) AS RANKNUM
+        //	FROM EMP
+        //)
 
-//select salary from rankedSalary
-//where RANKNUM =2;
+        //select salary from rankedSalary
+        //where RANKNUM =2;
+        Console.WriteLine("Hello World");
+    }
+}
