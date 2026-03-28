195c5a722f8a641a49be86a04693c293dab02928
Bishwa Raha <vishwaraha2014@gmail.com>
2026-03-28 21:02:33 +0530

    1. SecondLargestNumber into the ArrayList. 2. LINQ Interview Questions

---
+++ SecondLargestNumberArrayList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        int[] intArr = { 1, 2, 3, 4 };
+
+        int largest = int.MinValue;
+        int secondLargest = int.MinValue;
+
+        for (int i = 0; i < intArr.Length; i++)
+        {
+            if (intArr[i] > largest)
+            {
+                secondLargest = largest;
+                largest = intArr[i];
+            }
+            //else if (intArr[i] > secondLargest && intArr[i] != largest)
+            //{
+            //    secondLargest = intArr[i];
+            //}
+        }
+
+        Console.WriteLine("Second Largest Number: " + secondLargest);
+        Console.ReadLine();
+    }
+}
